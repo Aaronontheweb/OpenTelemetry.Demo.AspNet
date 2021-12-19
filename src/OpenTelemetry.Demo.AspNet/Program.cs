083c6226c4313f183cdf1a70aefde8110237e947
Aaron Stannard <aaron@petabridge.com>
2021-12-19 14:03:05 -0600

    finished demo

--- src/OpenTelemetry.Demo.AspNet/Program.cs
+++ src/OpenTelemetry.Demo.AspNet/Program.cs
@@ -1,7 +1,30 @@
+using OpenTelemetry.Demo.AspNet;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddOpenTelemetryTracing(b =>
+{
+    // uses the default Jaeger settings
+    b.AddJaegerExporter();
+    
+    // receive traces from our own custom sources
+    b.AddSource(TelemetryConstants.MyAppTraceSource);
+    
+    // decorate our service name so we can find it when we look inside Jaeger
+    b.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("AspNet", "Demo"));
+    
+    // receive traces from built-in sources
+    b.AddHttpClientInstrumentation();
+    b.AddAspNetCoreInstrumentation();
+    b.AddSqlClientInstrumentation();
+    
+    
+});
 
 var app = builder.Build();
 
@@ -12,6 +35,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
---
+++ src/OpenTelemetry.Demo.AspNet/TelemetryConstants.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+using OpenTelemetry.Trace;
+
+namespace OpenTelemetry.Demo.AspNet;
+
+public static class TelemetryConstants
+{
+    /// <summary>
+    /// The name of the <see cref="ActivitySource"/> that is going to produce our traces.
+    /// </summary>
+    public const string MyAppTraceSource = "Demo.AspNet";
+}
